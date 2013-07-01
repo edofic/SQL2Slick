65b0dd8418c5ff2d9df5bd75d8a6b1f730e1fcad
Andraz Bajt <edofic@gmail.com>
2013-07-01 22:01:21 +0200

    api for running conversion programmaticaly

--- src/main/scala/si/bss/tools/sql2slick/SqlToSlick.scala
+++ src/main/scala/si/bss/tools/sql2slick/SqlToSlick.scala
@@ -9,6 +9,20 @@ package si.bss.tools.sql2slick
 import StringCamelImplicits._
 
 object SqlToSlick {
+
+  def apply(sql: String): Either[String,String] = {
+    SqlFieldsParser.parseAll(SqlFieldsParser.table, sql) map {
+      case (tableName, fields) =>
+        val className = tableName.toCapitalizedCamelCaseIdent
+        val caseClass = SlickGenerator.genCaseClass(className, fields)
+        val mapped = SlickGenerator.genMappedTable(tableName,className,fields)
+        caseClass+"/n/n/n"+mapped
+    } match {
+      case SqlFieldsParser.Success(txt: String, _) => Right(txt)
+      case SqlFieldsParser.NoSuccess(txt, input) => Left(s"$txt; $input")
+    }
+  }
+
   def main(args: Array[String]) {
 
     println("--------------------------------------------------------------")
